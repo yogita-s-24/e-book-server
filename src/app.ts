e48290164eb69697e5d1c2239d470e48d60dc2fd
Yogita Shete <yogitashete24@gmail.com>
2024-07-16 15:55:19 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
-import express from 'express'
-
+import express, { NextFunction, Request, Response} from "express";
+import createHttpError from "http-errors";
+import globalErrorHandler from "./middlewares/globleErrorHandler";
 
 const app = express();
 
 //Routes
 //Http methods : GET, POST, PATCH, PUT, DELETE
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.get('/', (req, res, next)=>{
-    res.json({message  : "Welcome to elib apis."})
+app.get("/", (req, res, next) => {
+  
+  const error = createHttpError(400, "Something went to wrong.")
 
-})
+  throw error;
 
-export default app
+  res.json({ message: "Welcome to elib apis." });
+});
+
+
+//Global error handler
+app.use(globalErrorHandler)
+
+export default app;
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,8 @@ conf();
 
 const _config = {
     port: process.env.PORT,
-    databaseUrl:process.env.MONGODB_CONNECTION
+    databaseUrl:process.env.MONGODB_CONNECTION,
+    env:process.env.NODE_ENV,
 };
 
 export const config = Object.freeze(_config);
