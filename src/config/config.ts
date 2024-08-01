58419c423791852391149089bc07d0e6dd10e3d0
Yogita Shete <yogitashete24@gmail.com>
2024-08-01 12:45:05 +0530

    Add environment configuration using dotenv for secure credential management and cloud service integration

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,9 +4,18 @@ conf();
 
 const _config = {
     port: process.env.PORT,
-    databaseUrl:process.env.MONGODB_CONNECTION,
+
+    databaseUrl: process.env.MONGODB_CONNECTION,
+
     env:process.env.NODE_ENV,
-    jwtSecret: process.env.JWT_SECRET
+
+    jwtSecret: process.env.JWT_SECRET,
+
+    cloudinaryCloud: process.env.CLOUDINARY_CLOUD ,
+
+    cloudinaryApiKey: process.env.CLOUDINARY_API_KEY , 
+
+    cloudinarySecret: process.env.CLOUDINARY_API_SECRETE
 };
 
 export const config = Object.freeze(_config);
