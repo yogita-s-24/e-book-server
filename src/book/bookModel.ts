5c4cec446139651dc453f0e187757b8e42b62fff
Yogita Shete <yogitashete24@gmail.com>
2024-08-06 15:20:55 +0530

    ref : books schema

--- src/book/bookModel.ts
+++ src/book/bookModel.ts
@@ -9,6 +9,7 @@ const bookSchema = new mongoose.Schema<Book>(
     },
     author: {
       type: mongoose.Schema.Types.ObjectId,
+      ref : "User",
       required: true,
     },
     coverImage: {
