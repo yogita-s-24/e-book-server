4be627a5e79b894ca60b7a8b4696cd826359bde8
Yogita Shete <yogitashete24@gmail.com>
2024-08-06 15:20:17 +0530

    add type safety at authonticated middleware

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -6,17 +6,21 @@ import { config } from "../config/config";
 export interface AuthRequest extends Request {
   userId: string;
 }
+
 const authenticate = (req: Request, res: Response, next: NextFunction) => {
   const token = req.header("Authorization");
+
   if (!token) {
     return next(createHttpError(401, "Authorization token is required."));
   }
 
   try {
     const parsedToken = token.split(" ")[1];
-    const decoded = verify(parsedToken, config.jwtSecret as string);
+    const decoded = verify(parsedToken, config.jwtSecret as string) as {
+      sub: string;
+    };
     const _req = req as AuthRequest;
-    _req.userId = decoded.sub as string;
+    _req.userId = decoded.sub;
 
     next();
   } catch (err) {
